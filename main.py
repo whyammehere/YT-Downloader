8d519686ee815a763350ed014bb7aa372f7c4237
whyammehere <zhangian230791@gmail.com>
2023-02-03 13:16:48 -0500

    rewrite code

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from pytube import *
+# import modules
+from pytube import YouTube 
+import subprocess
+  
+# get link of the video
+link = input("Input video link: \n\n")
+  
+# download video
+uservid = YouTube(link).streams.filter().first().download(output_path = "videos/", filename = 'user_vid.mp4')
 
-vid = YouTube("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+# convert to audio file
+subprocess.run("ffmpeg -i videos/user_vid.mp4 videos/user_audio.mp3")
+
